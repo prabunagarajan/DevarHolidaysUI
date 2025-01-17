7cc5735df2a1883afb5c3a77ef3e2651631689d5
Santhosh <santhoshsangili.2802@gmail.com>
2025-01-17 17:56:51 +0530

    Vehicle Details Add

--- src/app/master/vehicle-details/add-edit-vehicle-details/add-edit-vehicle-details.component.ts
+++ src/app/master/vehicle-details/add-edit-vehicle-details/add-edit-vehicle-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-vehicle-details',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-vehicle-details.component.scss']
 })
 export class AddEditVehicleDetailsComponent implements OnInit {
-
-  constructor() { }
+  vehicleFormDetails: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.vehicleFormDetails = this.formBuilder.group({
+      fcDate: [''],
+      insuranceDate: [''],
+      polutionDate: [''],
+      remarks: [''],
+      status: [''],
+      taxDate: [''],
+      vehicleColor: [''],
+      vehicleName: [''],
+      vehicleNumber: [''],
+    });
   }
 
 }
