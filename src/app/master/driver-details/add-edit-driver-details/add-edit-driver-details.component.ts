f8bab2feadd80fb81bdd0c75d76dab7e8c2464b8
nsangeeth2002 <nsangeeth222@gmail.com>
2025-01-21 12:28:05 +0530

    Driver Details Add Edit List

--- src/app/master/driver-details/add-edit-driver-details/add-edit-driver-details.component.ts
+++ src/app/master/driver-details/add-edit-driver-details/add-edit-driver-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MasterService } from 'src/app/service/master.service';
 
 @Component({
   selector: 'app-add-edit-driver-details',
@@ -7,9 +11,177 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditDriverDetailsComponent implements OnInit {
 
-  constructor() { }
+  driverFormDetails!: FormGroup;
+  formSubmitted: boolean = false; // Initialize to false
+  @ViewChild('submitPopUp', { static: false }) submitPopUp;
+
+  screenName: String;
+  listId: any;
+  editDetails: any;
+  submitFlag: boolean;
+
+  constructor(
+    private fb: FormBuilder,
+    private masterService: MasterService,
+    private router: Router,
+    private toastr: ToastrService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.initializeForm();
+    this.screenName = 'Add';
+
+    this.activatedRoute.params.subscribe(routeResponse => {
+      if (routeResponse.id) {
+        this.listId = routeResponse.id;
+        this.submitFlag = true;
+        this.screenName = 'Edit';
+        this.masterService.driverDetailsView(routeResponse.id).subscribe(viewResponse => {
+          if (viewResponse.status == 's') {
+            this.editDetails = viewResponse.data;
+
+            this.driverFormDetails.patchValue({
+              aadharNumber: viewResponse.data.aadharNumber,
+              county: viewResponse.data.county,
+              district: viewResponse.data.district,
+              doorNumber: viewResponse.data.doorNumber,
+              drivingLicenseNumber: viewResponse.data.drivingLicenseNumber,
+              isPermanentDriver: viewResponse.data.isPermanentDriver ? 'Active' : 'Inactive',
+              mobileNumber: viewResponse.data.mobileNumber,
+              name: viewResponse.data.name,
+              state: viewResponse.data.state,
+              status: viewResponse.data.status ? 'Active' : 'Inactive',
+              street: viewResponse.data.street,
+              villageOrCity: viewResponse.data.villageOrCity
+            });
+
+          }
+        })
+      }
+    });
   }
+
+  initializeForm() {
+    this.driverFormDetails = this.fb.group({
+      aadharNumber: ['', [Validators.required]],
+      county: ['', Validators.required],
+      district: ['', Validators.required],
+      doorNumber: ['', Validators.required],
+      drivingLicenseNumber: ['', [Validators.required]],
+      isPermanentDriver: ['', [Validators.required]],
+      mobileNumber: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      state: ['', Validators.required],
+      status: ['', Validators.required],
+      street: ['', Validators.required],
+      villageOrCity: ['', Validators.required]
+    });
+  }
+
+  get driverForm(): { [key: string]: AbstractControl } {
+    return this.driverFormDetails.controls;
+  }
+
+  submit() {
+    if (this.driverFormDetails.valid) {
+      this.submitPopUp.show();
+    } else {
+      this.formSubmitted = true;
+    }
+  }
+
+  finalSubmit() {
+    if (this.submitFlag) {
+      this.updateDriverDetails();
+    } else {
+      this.addDriverDetails();
+    }
+  }
+
+  addDriverDetails() {
+    if (this.driverFormDetails.valid) {
+      const addDriverDetailsRequest = {
+        aadharNumber: this.driverFormDetails.value.aadharNumber,
+        county: this.driverFormDetails.value.county,
+        district: this.driverFormDetails.value.district,
+        doorNumber: this.driverFormDetails.value.doorNumber,
+        drivingLicenseNumber: this.driverFormDetails.value.drivingLicenseNumber,
+        isPermanentDriver: this.driverFormDetails.value.isPermanentDriver === 'Active',
+        mobileNumber: this.driverFormDetails.value.mobileNumber,
+        name: this.driverFormDetails.value.name,
+        state: this.driverFormDetails.value.state,
+        status: this.driverFormDetails.value.status === 'Active',
+        street: this.driverFormDetails.value.street,
+        villageOrCity: this.driverFormDetails.value.villageOrCity
+      };
+      this.masterService.driverDetailsAdd(addDriverDetailsRequest).subscribe(
+        (addDriverDetailsResponse: any) => {
+          if (addDriverDetailsResponse.status == 's') {
+            this.toastr.success(addDriverDetailsResponse.userDisplayMesg ? addDriverDetailsResponse.userDisplayMesg : 'Driver details submited successfully!');
+            this.router.navigate(['master/driver-detail/list']);
+          } else {
+            this.toastr.error(addDriverDetailsResponse.userDisplayMesg);
+          }
+        }
+      );
+    } else {
+      this.formSubmitted = true;
+    }
+  }
+
+  updateDriverDetails() {
+    if (this.driverFormDetails.valid) {
+      const updateDriverDetailsRequest = {
+        aadharNumber: this.driverFormDetails.value.aadharNumber,
+        county: this.driverFormDetails.value.county,
+        district: this.driverFormDetails.value.district,
+        doorNumber: this.driverFormDetails.value.doorNumber,
+        drivingLicenseNumber: this.driverFormDetails.value.drivingLicenseNumber,
+        isPermanentDriver: this.driverFormDetails.value.isPermanentDriver === 'Active',
+        mobileNumber: this.driverFormDetails.value.mobileNumber,
+        name: this.driverFormDetails.value.name,
+        state: this.driverFormDetails.value.state,
+        status: this.driverFormDetails.value.status === 'Active',
+        street: this.driverFormDetails.value.street,
+        villageOrCity: this.driverFormDetails.value.villageOrCity,
+        id: this.listId
+      };
+      this.masterService.driverDetailsUpdate(updateDriverDetailsRequest).subscribe(
+        (updateDriverDetailsResponse: any) => {
+          if (updateDriverDetailsResponse.status == 's') {
+            this.toastr.success(updateDriverDetailsResponse.userDisplayMesg ? updateDriverDetailsResponse.userDisplayMesg : 'Driver details updated successfully!');
+            this.router.navigate(['master/driver-detail/list']);
+          } else {
+            this.toastr.error(updateDriverDetailsResponse.userDisplayMesg);
+          }
+        }
+      );
+    } else {
+      this.formSubmitted = true;
+    }
+  }
+
+  onCancel() {
+    this.formSubmitted = false;
+    this.submitPopUp.hide();
+  }
+
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  allowOnlyLetters(event: KeyboardEvent) {
+    const pattern = /^[A-Za-z]$/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
 
 }
--- src/app/master/driver-details/driver-details.component.ts
+++ src/app/master/driver-details/driver-details.component.ts
@@ -1,15 +1,160 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MasterService } from 'src/app/service/master.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-driver-details',
   templateUrl: './driver-details.component.html',
   styleUrls: ['./driver-details.component.scss']
 })
-export class DriverDetailsComponent implements OnInit {
+export class DriverDetailsComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['serialNo', 'name', 'mobileNumber', 'drivingLicenseNumber', 'aadharNumber', 'district', 'status', 'action'];
+  dataSource: MatTableDataSource<any>;
+  viewEnable: boolean;
+  editEnable: boolean;
+  selectObj: any;
+  driverFormSearchDetails: FormGroup;
+  totelCount = 0;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  public pageSize = 10;
+
+  constructor(
+    private router: Router,
+    private masterService: MasterService,
+    private toastrMsg: ToastrService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.driverFormSearchDetails = this.formBuilder.group({
+      aadharNumber: [''],
+      drivingLicenseNumber: [''],
+      mobileNumber: [''],
+      driverName: ['']
+    });
+    this.getAll();
   }
 
-}
+  ngAfterViewInit() {
+    if (this.dataSource) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  getAll(pageIndex = 0, pageSize = this.pageSize) {
+    const driverFormSearchDetails = this.driverFormSearchDetails.value;
+    const request = {
+      filters: {
+        aadharNumber: driverFormSearchDetails.aadharNumber ? driverFormSearchDetails.aadharNumber : '',
+        drivingLicenseNumber: driverFormSearchDetails.drivingLicenseNumber ? driverFormSearchDetails.drivingLicenseNumber : '',
+        mobileNumber: driverFormSearchDetails.mobileNumber ? driverFormSearchDetails.mobileNumber : '',
+        name: driverFormSearchDetails.name ? driverFormSearchDetails.name : '',
+      },
+      paginationSize: pageSize,
+      sortField: "modifiedDate",
+      pageNo: pageIndex,
+      sortOrder: "DESC"
+    }
+    this.masterService.driverDetailsSearchList(request).subscribe(response => {
+      if (response.status == 's' && response.data) {
+        this.dataSource = new MatTableDataSource(response.data.contents);
+        this.totelCount = response.data.totalElements;
+      } else {
+        this.dataSource = new MatTableDataSource();
+      }
+    })
+  }
+
+  onclear() {
+
+    const request = {
+      filters: {
+        aadharNumber: '',
+        drivingLicenseNumber: '',
+        mobileNumber: '',
+        name: ''
+      },
+      paginationSize: 10,
+      sortField: "modifiedDate",
+      pageNo: 0,
+      sortOrder: "DESC"
+    }
+
+    this.masterService.driverDetailsSearchList(request).subscribe(response => {
+      if (response.status === 's' && response.data) {
+        this.dataSource = new MatTableDataSource(response.data.contents);
+        this.totelCount = response.data.totalElements;
+      } else {
+        this.dataSource = new MatTableDataSource();
+      }
+    });
+  }
+
+  search() {
+
+    const driverFormSearchDetails = this.driverFormSearchDetails.value;
+    const request = {
+      filters: {
+        aadharNumber: driverFormSearchDetails.aadharNumber ? driverFormSearchDetails.aadharNumber : '',
+        drivingLicenseNumber: driverFormSearchDetails.drivingLicenseNumber ? driverFormSearchDetails.drivingLicenseNumber : '',
+        mobileNumber: driverFormSearchDetails.mobileNumber ? driverFormSearchDetails.mobileNumber : '',
+        name: driverFormSearchDetails.name ? driverFormSearchDetails.name : '',
+      },
+      paginationSize: 10,
+      sortField: "modifiedDate",
+      pageNo: 0,
+      sortOrder: "DESC"
+    }
+
+    this.masterService.driverDetailsSearchList(request).subscribe(response => {
+      if (response.status === 's' && response.data) {
+        this.dataSource = new MatTableDataSource(response.data.contents);
+        this.totelCount = response.data.totalElements;
+      } else {
+        this.dataSource = new MatTableDataSource();
+      }
+    });
+  }
+
+  onSelect(obj) {
+    this.selectObj = obj ? obj : undefined;
+    if (obj) {
+      this.viewEnable = true;
+      this.editEnable = true;
+    }
+  }
+
+  onEdit() {
+    if (this.selectObj) {
+      this.router.navigate(['/master/driver-detail/modification', this.selectObj.id]);
+    } else {
+      this.toastrMsg.error('View not able');
+    }
+  }
+
+  onView() {
+    if (this.selectObj) {
+      this.router.navigate(['/master/driver-detail/view', this.selectObj.id]);
+    } else {
+      this.toastrMsg.error('View not able');
+    }
+  }
+
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  pageEvent(event) {
+    const pageIndex = event.pageIndex;
+    const pageSize = event.pageSize
+    this.getAll(pageIndex, pageSize);
+  }
+} 
