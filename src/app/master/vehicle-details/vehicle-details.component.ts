06ac6dc5f60485bbb3a775305ad4fb6ba49dee94
Santhosh <santhoshsangili.2802@gmail.com>
2025-01-21 17:49:42 +0530

    Vechile Details

--- src/app/master/vehicle-details/vehicle-details.component.ts
+++ src/app/master/vehicle-details/vehicle-details.component.ts
@@ -1,42 +1,82 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MasterService } from 'src/app/service/master.service';
 @Component({
   selector: 'app-vehicle-details',
   templateUrl: './vehicle-details.component.html',
   styleUrls: ['./vehicle-details.component.scss']
 })
 export class VehicleDetailsComponent implements OnInit {
-  ngOnInit() {
-    // throw new Error('Method not implemented.');
-  }
-  displayedColumns: string[] = ['serialNo', 'vehicleNo', 'vehicleName', 'driverName', 'district', 'phoneNo'];
-
-  // Define your data source
-  dataSource = new MatTableDataSource([
-    { vehicleNo: 'TN-01-1234', vehicleName: 'Honda City', driverName: 'John Doe', district: 'Chennai', phoneNo: '9876543210' },
-    { vehicleNo: 'TN-02-5678', vehicleName: 'Toyota Corolla', driverName: 'Jane Doe', district: 'Madurai', phoneNo: '9876543211' },
-    { vehicleNo: 'TN-03-9101', vehicleName: 'BMW X5', driverName: 'Mark Smith', district: 'Coimbatore', phoneNo: '9876543212' },
-    { vehicleNo: 'TN-04-1121', vehicleName: 'Mercedes Benz', driverName: 'Sara Lee', district: 'Salem', phoneNo: '9876543213' },
-    { vehicleNo: 'TN-05-3141', vehicleName: 'Audi A6', driverName: 'Paul Smith', district: 'Trichy', phoneNo: '9876543214' },
-    { vehicleNo: 'TN-06-5161', vehicleName: 'Ford Mustang', driverName: 'Anna Brown', district: 'Tirunelveli', phoneNo: '9876543215' },
-    { vehicleNo: 'TN-07-7181', vehicleName: 'Chevrolet Cruze', driverName: 'Lucy Green', district: 'Vellore', phoneNo: '9876543216' },
-    { vehicleNo: 'TN-08-9202', vehicleName: 'Nissan Altima', driverName: 'Tom White', district: 'Kanchipuram', phoneNo: '9876543217' },
-    { vehicleNo: 'TN-01-1234', vehicleName: 'Honda City', driverName: 'John Doe', district: 'Chennai', phoneNo: '9876543210' },
-    { vehicleNo: 'TN-02-5678', vehicleName: 'Toyota Corolla', driverName: 'Jane Doe', district: 'Madurai', phoneNo: '9876543211' },
-    { vehicleNo: 'TN-03-9101', vehicleName: 'BMW X5', driverName: 'Mark Smith', district: 'Coimbatore', phoneNo: '9876543212' },
-    { vehicleNo: 'TN-04-1121', vehicleName: 'Mercedes Benz', driverName: 'Sara Lee', district: 'Salem', phoneNo: '9876543213' },
-    { vehicleNo: 'TN-05-3141', vehicleName: 'Audi A6', driverName: 'Paul Smith', district: 'Trichy', phoneNo: '9876543214' },
-    { vehicleNo: 'TN-06-5161', vehicleName: 'Ford Mustang', driverName: 'Anna Brown', district: 'Tirunelveli', phoneNo: '9876543215' },
-    { vehicleNo: 'TN-07-7181', vehicleName: 'Chevrolet Cruze', driverName: 'Lucy Green', district: 'Vellore', phoneNo: '9876543216' },
-    { vehicleNo: 'TN-08-9202', vehicleName: 'Nissan Altima', driverName: 'Tom White', district: 'Kanchipuram', phoneNo: '9876543217' },
-  ]);
-
-  // ViewChild for paginator
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['serialNo', 'vehicleNo', 'vehicleName', 'insuranceDate', 'taxDate', 'fcDate', 'polutionDate', 'action'];
+  selectedRecord: any;
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  totalCounts: any;
+  viewDisabled: boolean;
+  editDisabled: boolean;
+  public pageSize = 10;
+  constructor(private driverService: MasterService,
+    private toaster: ToastrService,
+    private router: Router) { }
 
-  // After view initialization, set paginator to dataSource
+  ngOnInit() {
+    this.loadDriverDetails();
+  }
+
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+
+  loadDriverDetails(pageIndex = 0, pageSize = this.pageSize) {
+    const request = {
+      filters: {},
+      pageNo: pageIndex,
+      paginationSize: pageSize,
+      sortField: 'modifiedDate',
+      sortOrder: 'DESC',
+    };
+
+    this.driverService.driverDetailsGetAllList(request).subscribe(
+      (res) => {
+        if (res.status === 's') {
+          this.dataSource.data = res.data.contents;
+          this.totalCounts = res.data.totalElements;
+        } else {
+          console.error('Failed to fetch driver details');
+        }
+      },
+      (error) => {
+        console.error('Error fetching driver details:', error);
+      }
+    );
+  }
+
+  selectRecord(record) {
+    this.selectedRecord = record ? record : undefined;
+    if (this.selectedRecord) {
+      this.editDisabled = true;
+      this.viewDisabled = true;
+    }
+  }
+
+  edit() {
+    if (this.selectedRecord) {
+      this.router.navigate(['/master/vehicle-detail/modification', this.selectedRecord.id]);
+    }
+  }
+
+  view() {
+    if (this.selectedRecord) {
+      this.router.navigate(['/master/vehicle-detail/view', this.selectedRecord.id]);
+    }
+  }
+
+  pageEvent(event) {
+    const pageIndex = event.pageIndex;
+    const pageSize = event.pageSize
+    this.loadDriverDetails(pageIndex, pageSize);
   }
 }
