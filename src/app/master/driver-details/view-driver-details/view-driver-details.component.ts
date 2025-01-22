b0948a542ab49220e09c60be100c5634151f105f
nsangeeth2002 <nsangeeth222@gmail.com>
2025-01-22 17:15:44 +0530

    Driver Details View Completed

--- src/app/master/driver-details/view-driver-details/view-driver-details.component.ts
+++ src/app/master/driver-details/view-driver-details/view-driver-details.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, } from '@angular/core';
+import {  FormBuilder,  FormGroup,  Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MasterService } from 'src/app/service/master.service';
 @Component({
   selector: 'app-view-driver-details',
   templateUrl: './view-driver-details.component.html',
   styleUrls: ['./view-driver-details.component.scss']
 })
 export class ViewDriverDetailsComponent implements OnInit {
+  driverDetails: any;
+  
+constructor(private masterService: MasterService,
+  private activatedRoute: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit() {
+ngOnInit() {
+  this.activatedRoute.params.subscribe(getDriverByIdResponse => {
+    if (getDriverByIdResponse.id) {
+      this.getDriverById(getDriverByIdResponse.id)
+    }
+  })
+}
+  
+  getDriverById(viewId) {
+    this.masterService.getDriverById(viewId).subscribe(getDriverByIdResponse => {
+      if (getDriverByIdResponse.status == 's') {
+        this.driverDetails = getDriverByIdResponse.data;
+      }
+    });
   }
-
 }
