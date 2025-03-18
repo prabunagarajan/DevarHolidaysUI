a94d6e416701337c81445be3bc05284541ece989
Santhosh <santhoshsangili.2802@gmail.com>
2025-03-18 17:39:27 +0530

    Driver Payment

---
+++ src/app/container/driver-payment/driver-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-payment',
+  templateUrl: './driver-payment.component.html',
+  styleUrls: ['./driver-payment.component.scss']
+})
+export class DriverPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
