0d0000f6b74b85a39e31903f3779763fdc3b2d6d
sivaraajc <sivaraaj6997@gmail.com>
2025-03-27 15:26:27 +0530

    DashBoard Design Changes

--- src/app/demo/dashboard/dash-analytics/dash-analytics.component.ts
+++ src/app/demo/dashboard/dash-analytics/dash-analytics.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDB } from '../../../fack-db/chart-data';
-import {ApexChartService} from '../../../theme/shared/components/chart/apex-chart/apex-chart.service';
+import { ApexChartService } from '../../../theme/shared/components/chart/apex-chart/apex-chart.service';
+import { CommonService } from 'src/app/service/common.service';
 
 @Component({
   selector: 'app-dash-analytics',
@@ -13,7 +14,32 @@ export class DashAnalyticsComponent implements OnInit {
   public dailyVisitorAxis: any;
   public deviceProgressBar: any;
 
-  constructor(public apexEvent: ApexChartService) {
+  places = [
+    {
+      image: 'assets/CAR/CAR.jpeg'
+    },
+    {
+      image: 'assets/CAR/CAR1.jpeg'
+    },
+    {
+      image: 'assets/CAR/CAR3.jpeg'
+    },
+    {
+      image: 'assets/CAR/CAR4.jpeg'
+    },
+    {
+      image: 'assets/CAR/CAR5.jpeg'
+    },
+    {
+      image: 'assets/CAR/CAR6.jpeg'
+    },
+    {
+      image: 'assets/CAR/CAR7.jpeg'
+    },
+  ];
+  totalVehicleTripsAndProfit: any;
+
+  constructor(public apexEvent: ApexChartService, private commonservice: CommonService) {
     this.chartDB = ChartDB;
     this.dailyVisitorStatus = '1y';
 
@@ -31,6 +57,22 @@ export class DashAnalyticsComponent implements OnInit {
     ];
   }
 
-  ngOnInit() {}
+  driverTrips: any[] = [];
 
+
+  ngOnInit() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    this.commonservice.dashBoardCount(month, year).subscribe(res => {
+      this.driverTrips = res;
+      console.log(this.driverTrips);
+
+    })
+    this.commonservice.getTotalVehicleTripsAndProfit(month, year).subscribe(res => {
+      this.totalVehicleTripsAndProfit = res;
+      console.log(this.driverTrips);
+
+    })
+  }
 }
